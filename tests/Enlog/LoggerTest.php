ed6800a0cc0294cf16bf3936bba9b88d5b0e3da9
linchong899201 <linchong@hotmail.com>
2017-11-06 17:06:32 +0800

    Added configuration and bootstrap

---
+++ tests/Enlog/LoggerTest.php
@@ -0,0 +1,24 @@
+<?php 
+
+class LoggerTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * #var $logger \Enlog\Logger
+	 */
+	
+	protected $logger;
+
+	public function setUp()
+	{
+		$this->logger = new \Enlog\Logger();
+	}
+
+	public function testInstance()
+	{
+		$this->assertInstanceOf('\Enlog\Logger',$this->logger);
+
+	}
+}
+
+
+ ?>
