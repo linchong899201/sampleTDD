194c9dde9910c4b8ab08e388b59dd1d7dd7a7c8e
linchong899201 <linchong@hotmail.com>
2017-11-07 12:10:55 +0800

    A Simple Logger Using TDD

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+
+use \Enlog\Logger;
+use \Enlog\Handler\MemoryHandler;
+
+$logger = new Logger();
+$handler = new MemoryHandler();
+
+$logger->registerHandler('memory',$handler);
+
+$logger->log("Yay,I wrote first simple library using TDD!");
+
+var_dump($handler->getEntries());
+
+ ?>
